656715b893b5cc7bfbd7fb812ffc3c59b60fce23
Esti <esti@estimacbookair.local>
2021-12-14 19:41:19 +0000

    paths

--- utils/prepare_data.py
+++ utils/prepare_data.py
@@ -10,8 +10,8 @@ import numpy as np
 # s = sys.argv[2]
 
 # name of the input folder
-main_path = ""
-folder = "input"
+main_path = '/Volumes/OCB-All/Projects/OCB004_Phototoxicity/Data/NikonTi/2021-12-06'
+folder = "videos"
 # name of the new folder which will be store in the same directory as the input folder.
 new_folder = folder + "_2d_scaled"
 # scaling factor
@@ -20,10 +20,10 @@ s = 10
 if not os.path.exists(os.path.join(main_path, new_folder)):
     os.mkdir(os.path.join(main_path, new_folder))
 
-for f in os.listdir(folder):
+for f in os.listdir(os.path.join(main_path, folder)):
     print(f)
     # Read the image
-    seq = tifffile.imread(os.path.join(folder, f))
+    seq = tifffile.imread(os.path.join(main_path, folder, f))
     seq = seq.astype(np.uint16)
     print(seq.shape)
     for t in range(seq.shape[0]):
@@ -35,8 +35,8 @@ for f in os.listdir(folder):
         else:
             image_resized = cv2.resize(im, dsize=(im.shape[0] // s, im.shape[1] // s), interpolation=cv2.INTER_CUBIC)
         # Use this function only in StarDist notebooks.
-        # save_tiff_imagej_compatible(os.path.join(new_folder, f.split(".tif")[0] + "_{:02d}.tif".format(t)), image_resized, "yx")
+        # save_tiff_imagej_compatible(os.path.join(main_path, new_folder, f.split(".tif")[0] + "_{:02d}.tif".format(t)), image_resized, "yx")
 
-        tifffile.imsave(os.path.join(new_folder, f.split(".tif")[0] + "_{:02d}.tif".format(t)),
+        tifffile.imsave(os.path.join(main_path, new_folder, f.split(".tif")[0] + "_{:04d}.tif".format(t)),
                         image_resized, imagej=True)
 
