1ffe780945b669cff71a6a2241b9882a2803fac7
esgomezm <esti@estimacbookair.local>
2022-03-24 14:41:17 +0000

    typo

--- accesorial_scripts/prepare_masks_annotation.py
+++ accesorial_scripts/prepare_masks_annotation.py
@@ -6,13 +6,13 @@ import cv2
 from tifffile import imread, imsave
 from utils.morphology import smooth_labels
 
-files = {"2021-12-06": {
-                        "11": 5,
-                        "14": 6,
-                        "12": 3,
-                        "15": 4
-                                },
-        "2021-11-23": {
+# files = {"2021-12-06": {
+#                         "11": 5,
+#                         "14": 6,
+#                         "12": 3,
+#                         "15": 4
+#                                 },
+files = {       "2021-11-23": {
                         "07": 6,
                         "14": 14,
                         "16": 15,
@@ -21,6 +21,7 @@ files = {"2021-12-06": {
         }
 
 s = 8
+size=7885
 main_path = "/Users/esti/Documents/PHX/ground_truth/masks"
 output_path = "/Users/esti/Documents/PHX/ground_truth/upsampled_masks"
 if not os.path.exists(output_path):
@@ -33,7 +34,7 @@ for f in files.keys():
         video_mask = imread(os.path.join(main_path, f, v+'.tif'))
         for i in range(files[f][v]-1, files[f][v]+1):
             frame = video_mask[i]
-            frame = cv2.resize(frame, dsize=(int(frame.shape[0] * s), int(frame.shape[1] * s)),
+            frame = cv2.resize(frame, dsize=(size, size),
                                interpolation=cv2.INTER_NEAREST)
             frame = smooth_labels(frame)
             imsave(os.path.join(output_path, f + "_" + v + "_{:03d}.tif".format(i+1)),
