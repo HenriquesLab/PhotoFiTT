5975a862fcf0cc3f4c2b571f7eaa717fd8265526
esgomezm <esti@estimacbookair.home>
2022-11-13 13:16:31 +0000

    add crosscorr

--- analysis/dynamics/analyse_fov_motility.py
+++ analysis/dynamics/analyse_fov_motility.py
@@ -3,13 +3,14 @@ import matplotlib.pyplot as plt
 import os
 import sys
 import numpy as np
+
 ## Include the following lines to access the code in Python Console
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 SCRIPT_DIR = '/Users/esti/Documents/PROYECTOS/PHX/mitosis-mediated-phototoxic'
 sys.path.append(SCRIPT_DIR)
 from utils.fov_motility import extract_dynamics_metrics
-from utils.display import plot_smooth_curves,  plot_conditions_with_aggregates
+from utils.display import plot_smooth_curves, plot_conditions_with_aggregates
 
 main_path = "/Users/esti/Documents/PROYECTOS/PHX/mitosis_mediated_data_itqb_3/masks/scaled_1.5709_results/stardist_prob03/"
 output_path = "/Users/esti/Documents/PROYECTOS/PHX/mitosis_mediated_data_itqb_3/results/scaled_1.5709_results/stardist_prob03"
@@ -17,14 +18,16 @@ folder = "dynamics"
 if not os.path.exists(os.path.join(output_path, folder)):
     os.mkdir(os.path.join(output_path, folder))
 
-dynamics_metrics = extract_dynamics_metrics(main_path, save_steps=False)
-dynamics_metrics.to_csv(os.path.join(output_path, folder, "data_dynamics.csv"))
+method = "cross-correlation"
+dynamics_metrics = extract_dynamics_metrics(main_path, method=method, save_steps=False)
+dynamics_metrics.to_csv(os.path.join(output_path, folder, "data_dynamics_{}.csv".format(method)))
 
 for c in np.unique(dynamics_metrics["Subcategory-01"]):
-    dynamics_group = dynamics_metrics[dynamics_metrics["Subcategory-01"]==c].reset_index(drop=True)
+    dynamics_group = dynamics_metrics[dynamics_metrics["Subcategory-01"] == c].reset_index(drop=True)
 
-    plot_smooth_curves(dynamics_group, "mitoses", "Dynamics_variance_{}".format(c),
-                       os.path.join(output_path, folder), "Dynamics_variance_1_{}.png".format(c))
-    plot_conditions_with_aggregates(dynamics_group, "mitoses",  "Dynamics_variance_{}".format(c),
-                                    os.path.join(output_path, folder), "Dynamics_variance_2_{}.png".format(c),
-                                    hue="Subcategory-02", style="Subcategory-01")
+    # plot_smooth_curves(dynamics_group, "time_variance", "Dynamics_variance_{}".format(c),
+    #                    os.path.join(output_path, folder), "Dynamics_variance_1_{}.png".format(c))
+    plot_conditions_with_aggregates(dynamics_group, "time_variance", "Dynamics_{0}_variance_{1}".format(method, c),
+                                    os.path.join(output_path, folder),
+                                    "Dynamics_{0}_variance_2_{1}.png".format(method, c), hue="Subcategory-02",
+                                    style="Subcategory-01")
--- utils/fov_motility.py
+++ utils/fov_motility.py
@@ -3,6 +3,13 @@ from tifffile import imread, imsave
 import pandas as pd
 from utils.normalisation import normalise_phc_timelapse
 import os
+import sys
+SCRIPT_DIR = "/Users/esti/Documents/PROYECTOS/PHX/NanoPyx/src"
+sys.path.append(SCRIPT_DIR)
+from enanoscopy.methods.image.transform.cross_correlation_map import CrossCorrelationMap
+
+
+
 
 def time_intensity_variability(im):
     diff = im[:-1] - im[1:]
@@ -10,7 +17,14 @@ def time_intensity_variability(im):
     mean_variability = [np.mean(diff[t]) for t in range(diff.shape[0])]
     return mean_variability, diff
 
-def extract_dynamics_metrics(path, dynamics_info=None, column_data=[], frame_rate=4, save_steps=False, output_path=''):
+def time_crosscorrelation_variability(im):
+    xc = CrossCorrelationMap()
+    xc_time = [xc.calculate_ccm(im[i], im[i+1], normalize=True) for i in range(im.shape[0]-1)]
+    xc_mean = [np.mean(x) for x in xc_time]
+    xc_time = np.array(xc_time)
+    return xc_mean, xc_time
+
+def extract_dynamics_metrics(path, dynamics_info=None, column_data=[], frame_rate=4, method="intensity", save_steps=False, output_path=''):
     folders = os.listdir(path)
     folders.sort
     print(folders)
@@ -22,12 +36,15 @@ def extract_dynamics_metrics(path, dynamics_info=None, column_data=[], frame_rat
                     os.makedirs("path/to/directory", exist_ok=True)
                 dynamics_info = extract_dynamics_metrics(os.path.join(path, f), dynamics_info=dynamics_info,
                                                          column_data=column_data + [f], frame_rate=frame_rate,
-                                                         save_steps=save_steps, output_path=os.path.join(output_path, f))
+                                                         method=method, save_steps=save_steps, output_path=os.path.join(output_path, f))
             elif f.__contains__('.tif'):
                 print(f)
                 im = imread(os.path.join(path, f))
                 new_im = normalise_phc_timelapse(im)
-                dynamics_val, diff = time_intensity_variability(new_im)
+                if method == "cross-correlation":
+                    dynamics_val, diff = time_crosscorrelation_variability(new_im)
+                elif method == "intensity":
+                    dynamics_val, diff = time_intensity_variability(new_im)
                 if save_steps:
                     os.makedirs(os.path.join(output_path, f), exist_ok=True)
                     imsave(os.path.join(output_path, "normalised_" + f), new_im)
