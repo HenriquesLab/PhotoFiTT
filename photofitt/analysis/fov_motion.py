ab2021ce3d0dd78d0ef46bd440c6d75a29bf68e6
Esti <esti@estimacbookair.local>
2023-06-19 15:51:16 -0400

    cummulative motion function

--- photofitt/analysis/fov_motion.py
+++ photofitt/analysis/fov_motion.py
@@ -129,3 +129,63 @@ def extract_motion(path, motion_info=None, column_data=[], frame_rate=4, enhance
                                      "data_motion_{0}_{1}_temp.csv".format(method, condition)))
 
     return motion_info
+
+
+def cummulative_motion(motion_metrics, use_starting_point=None, starting_point=0, data_peaks=None):
+    """
+
+    :param use_starting_point: One between None, "event peak", or "fixed". None by default
+    :param starting_point:  The time-point from which the cummulation will be done.
+                            If set to 0 will be the same effect as use_starting_point=None
+    :param data_peaks: pandas dataframe in which the peak of mitotic events are stored.
+    :return: motion_dataframe, motion_metrics:
+    """
+
+    # Initialise the parameters
+    assert isinstance(use_starting_point,
+                      str), 'cummulative_motion() parameter use_starting_point={} not of <class "str">'.format(
+        use_starting_point)
+    assert starting_point >= 0, f"starting_point >= 0 expected, got: {starting_point}"
+
+    motion_metrics["Cummulative cell motion"] = 0
+    motion = []
+
+    # Cover each folder and each video to process each independent acquisition
+    for f in np.unique(motion_metrics["Subcategory-00"]):
+        # Create a new data set to filter the experimental replica.
+        motion_metrics_f = motion_metrics[motion_metrics["Subcategory-00"] == f]
+
+        # Process each video acquired on the day f
+        for v in np.unique(motion_metrics_f["video_name"]):
+            motion_metrics_fv = motion_metrics_f[motion_metrics_f["video_name"] == v]
+            if use_starting_point is not None:
+                if use_starting_point == "event peak":
+                    # Recover the time point at which the peak is achieved
+                    data_cf = data_peaks[data_peaks["Subcategory-00"] == f].reset_index(drop=True)
+                    data_cfv = data_cf[data_cf["video_name"] == v].reset_index(drop=True)
+                    starting_point = data_cfv["Peak time point (min)"].iloc[0]
+
+                # Compute the cummulative cell growth only after the mitosis
+                after_aux = motion_metrics_fv[motion_metrics_fv["frame"] > starting_point]
+                index = after_aux.index.to_list()
+                cummulative_motion = after_aux["Cell motion"].cumsum()
+                motion_metrics.loc[index, ["Cummulative cell motion"]] = cummulative_motion
+                # Average the cell growth
+                mean_motion = np.mean(after_aux["Cell motion"])
+            else:
+                index = motion_metrics_fv.index.to_list()
+                cummulative_motion = motion_metrics["Cell motion"].cumsum()
+                motion_metrics.loc[index, ["Cummulative cell motion"]] = cummulative_motion
+                # Average the cell growth
+                mean_motion = np.mean(motion_metrics["Cell motion"])
+            # Estimate the slope
+            cummulative_motion = np.array(cummulative_motion)
+            frame = np.array(motion_metrics.loc[index, ["frame"]])
+            slope = (cummulative_motion[1:] - cummulative_motion[:-1]) / (frame[1:] - frame[:-1])
+            slope = np.mean(slope)
+            motion.append([mean_motion, slope, v, f, motion_metrics_fv["Subcategory-01"].iloc[0],
+                           motion_metrics_fv["Subcategory-02"].iloc[0]])
+
+    motion_dataframe = pd.DataFrame(motion, columns=['Mean motion', 'Motion slope', "video_name",
+                                                     'Subcategory-00', 'Subcategory-01', "Subcategory-02"])
+    return motion_dataframe, motion_metrics
