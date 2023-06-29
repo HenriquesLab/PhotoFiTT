ff87523151123d5bce31fdb05b88692909134525
esgomezm <esti@estimacbookair.igc.gulbenkian.pt>
2023-06-29 15:12:12 +0100

    cummulative motion

--- photofitt/analysis/__init__.py
+++ photofitt/analysis/__init__.py
@@ -1,3 +1,3 @@
-from .fov_motion import extract_motion
+from .fov_motion import extract_motion, cummulative_motion
 from .mitosis_counting import count_mitosis_all, quantify_peaks, compare_peaks
 from .statistics import data_statistics, run_fitting, GaussianMixtureTime, extract_gaussian_params, extract_gaussian_params_videowise
