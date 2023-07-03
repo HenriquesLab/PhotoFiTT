47916eb7cdff3de72023b8f46fc04322d4ae7640
esgomezm <esti@estimacbookair.igc.gulbenkian.pt>
2023-07-03 14:16:08 +0100

    args fov

--- analysis/motion/extract_fov_motility.py
+++ analysis/motion/extract_fov_motility.py
@@ -14,43 +14,39 @@ import sys
 from photofitt.analysis import extract_motion
 from photofitt.display import smooth_curves, conditions_with_aggregates
 
-
-
-
-main_path = sys.argv[1]
-output_path = sys.argv[2]
-condition = sys.argv[3] # "630", "WL UV - high..."
-
-
-
 # main_path = "/Users/esti/Documents/PROYECTOS/PHX/mitosis_mediated_data_itqb_3/inputs/scaled_1.5709_results/2022-08-10/"
 # main_path = "/Users/esti/Documents/PROYECTOS/PHX/mitosis_mediated_data_itqb_3/inputs/scaled_1.5709_results/2022-08-10/WL UV - high density/Synchro"
 # output_path = "/Users/esti/Documents/PROYECTOS/PHX/mitosis_mediated_data_itqb_3/results/scaled_1.5709_results/stardist_prob03"
 
+main_path = sys.argv[1]
+output_path = sys.argv[2]
+condition = sys.argv[3]  # "630", "WL UV - high..."
+wl = sys.argv[4]
+method = sys.argv[5]  # "intensity"
 
-method = sys.argv[4] # "intensity"
+if condition != "None":
+    pass
+
+else:
+    condition = None
 
 folder = "motion_clahe-{}".format(method)
 if not os.path.exists(os.path.join(output_path, folder)):
     os.mkdir(os.path.join(output_path, folder))
 
 motion_metrics = extract_motion(main_path, method=method, save_steps=False, enhance_contrast=True,
-                                            output_path=os.path.join(output_path, folder), condition=condition)
+                                output_path=os.path.join(output_path, folder), condition=condition)
 print(motion_metrics)
-motion_metrics.to_csv(os.path.join(output_path, folder, "data_motion_{0}_{1}.csv".format(method, condition)))
-conditions_with_aggregates(motion_metrics, "time_variance", "Motion_{0}_variance_{1}".format(method, condition),
-                                os.path.join(output_path, folder),
-                                "Motion_{0}_variance_0_{1}.png".format(method, condition), hue="Subcategory-02",
-                                style="Subcategory-01")
-#
-# for c in np.unique(motion_metrics["Subcategory-01"]):
-#     motion_group = motion_metrics[motion_metrics["Subcategory-01"] == c].reset_index(drop=True)
-#
-#     # smooth_curves(motion_group, "time_variance", "Motion_variance_{}".format(c),
-#     #                    os.path.join(output_path, folder), "Motion_variance_1_{}.png".format(c))
-#     conditions_with_aggregates(motion_group, "time_variance", "Motion_{0}_variance_{1}".format(method, c),
-#                                     os.path.join(output_path, folder),
-#                                     "Motion_{0}_variance_2_{1}.png".format(method, c), hue="Subcategory-02",
-#                                     style="Subcategory-01")
-#
-#
+if wl == "None" or wl==None:
+    motion_metrics.to_csv(os.path.join(output_path, folder, "data_motion_{0}.csv".format(method)))
+    conditions_with_aggregates(motion_metrics, "time_variance", "Motion_{0}_variance".format(method),
+                               os.path.join(output_path, folder),
+                               "Motion_{0}_variance_0.png".format(method), hue="Subcategory-02",
+                               style="Subcategory-01")
+else:
+
+    motion_metrics.to_csv(os.path.join(output_path, folder, "data_motion_{0}_{1}.csv".format(method, wl)))
+    conditions_with_aggregates(motion_metrics, "time_variance", "Motion_{0}_variance_{1}".format(method, wl),
+                               os.path.join(output_path, folder),
+                               "Motion_{0}_variance_0_{1}.png".format(method, wl), hue="Subcategory-02",
+                               style="Subcategory-01")
